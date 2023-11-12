c1ee27160407448008f2555fc9396c8e47c72648
Tyler CB <143175038+808TYLERCB@users.noreply.github.com>
2023-11-12 02:13:28 -1000

    update

--- Public/products.js
+++ Public/products.js
@@ -1,31 +1,94 @@
-// products.js (assuming this file is linked in your HTML and served from the public directory)
-function displayProducts(products) {
-    let htmlContent = '<div class="row">'; // Start the first row
-    products.forEach((product, index) => {
-        // Add product card in a column
-        htmlContent += `
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="${product.image}" alt="${product.name}">
-                    <div class="card-body">
-                        <h5 class="card-title">${product.name}</h5>
-                        <p class="card-text">${product.description}</p>
-                        <p class="card-text">Price: $${product.price}</p>
-                        <p class="card-text">Available: ${product.qty_available}</p>
-                        <input type="text" name="${product.name}" class="form-control" placeholder="Enter quantity">
-                    </div>
-                </div>
-            </div>
-        `;
+// Fetch the product data from the server
+fetch('/products.json')
+    .then(response => response.json())
+    .then(products => {
+        // Get the element where you want to display the products
+        const productsContainer = document.getElementById('products-container');
 
-        // Every 3 products, close the current row and start a new one
-        if ((index + 1) % 3 === 0) {
-            htmlContent += '</div><div class="row">';
-        }
+        // Create a row for the products
+        let row = createRow();
+        productsContainer.appendChild(row);
+
+        // Loop through each product
+        products.forEach((product, index) => {
+            // Create a Bootstrap card for each product
+            const card = createCard(product);
+
+            // Add the card to the current row
+            row.appendChild(card);
+
+            // Every 3 products, create a new row
+            if ((index + 1) % 3 === 0 && index + 1 < products.length) {
+                row = createRow();
+                productsContainer.appendChild(row);
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching products:', error);
     });
 
-    htmlContent += '</div>'; // Close the last row
-    document.getElementById('products-container').innerHTML = htmlContent;
+// Function to create a new row
+function createRow() {
+    const rowDiv = document.createElement('div');
+    rowDiv.className = 'row mb-4';
+    return rowDiv;
 }
 
+// Function to create a Bootstrap card for a product
+function createCard(product) {
+    const colDiv = document.createElement('div');
+    colDiv.className = 'col-md-4';
 
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+
+    const img = document.createElement('img');
+    img.className = 'card-img-top';
+    img.src = product.image;
+    img.alt = product.name;
+    cardDiv.appendChild(img);
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const title = document.createElement('h5');
+    title.className = 'card-title';
+    title.textContent = product.name;
+    cardBody.appendChild(title);
+
+    const price = document.createElement('p');
+    price.className = 'card-text';
+    price.textContent = `Price: $${product.price}`;
+    cardBody.appendChild(price);
+
+    const qty = document.createElement('p');
+    qty.className = 'card-text';
+    qty.textContent = `Quantity Available: ${product.qty_available}`;
+    cardBody.appendChild(qty);
+
+    const description = document.createElement('p');
+    description.className = 'card-text';
+    description.textContent = product.description;
+    cardBody.appendChild(description);
+
+    // Purchase section with quantity input and purchase button
+    const purchaseDiv = document.createElement('div');
+    purchaseDiv.className = 'purchase-section d-flex align-items-center justify-content-between mt-2';
+
+    const quantityInput = document.createElement('input');
+    quantityInput.type = 'number';
+    quantityInput.className = 'form-control';
+    quantityInput.value = 0; // default value
+    quantityInput.min = 1; // minimum value
+    quantityInput.max = product.qty_available; // maximum value
+    purchaseDiv.appendChild(quantityInput);
+
+
+    cardBody.appendChild(purchaseDiv);
+
+    cardDiv.appendChild(cardBody);
+    colDiv.appendChild(cardDiv);
+
+    return colDiv;
+}
--- server.js
+++ server.js
@@ -10,22 +10,92 @@ app.use((request, response, next) => {
     next();
 });
 
+// Middleware to parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
 // Import data from a JSON file containing information about products
-// Note: Changed the path to correctly point to "products.json"
 const products = require(__dirname + "/products.json");
 
 // Define a route for handling a GET request to "/products.js"
-// Note: Corrected the path to start with a "/"
 app.get('/products.js', (request, response) => {
-    // Set the response content type to JavaScript
     response.type('js');
-
-    // Convert the products object to a JSON string and embed it within a JavaScript variable
     const productsStr = `let products = ${JSON.stringify(products)};`;
-
-    // Send the JavaScript string in response to the GET request
     response.send(productsStr);
 });
+
+app.post('/purchase', (req, res) => {
+    const purchaseData = req.body; // Assuming this is an object with product IDs and quantities
+
+    // Validate purchase data
+    let isValidPurchase = true;
+    let errorMessage = '';
+    let totalQuantity = 0;
+
+    for (let productId in purchaseData) {
+        const quantity = parseInt(purchaseData[productId], 10);
+
+        if (isNaN(quantity) || quantity < 0) {
+            isValidPurchase = false;
+            errorMessage = 'Invalid quantity entered.';
+            break;
+        }
+
+        if (quantity > products[productId].qty_available) {
+            isValidPurchase = false;
+            errorMessage = 'Quantity exceeds available stock.';
+            break;
+        }
+
+        if (quantity === 0) {
+            continue; // Skip if quantity is 0, but keep checking other items
+        }
+
+        totalQuantity += quantity;
+    }
+
+    if (totalQuantity === 0) {
+        isValidPurchase = false;
+        errorMessage = 'No quantities selected.';
+    }
+
+    if (!isValidPurchase) {
+        // Redirect back to products page with an error message
+        return res.redirect('/products_display.html?error=' + encodeURIComponent(errorMessage));
+    }
+
+    // Update inventory and calculate total
+    let total = 0;
+    for (let productId in purchaseData) {
+        const quantity = purchaseData[productId];
+        products[productId].qty_available -= quantity;
+        total += products[productId].price * quantity;
+    }
+
+    // Calculate tax at 4%
+    const tax = total * 0.04;
+
+    // Calculate shipping based on total quantity
+    const shipping = calculateShipping(totalQuantity);
+
+    const grandTotal = total + tax + shipping;
+
+    // Generate invoice
+    res.send(`Invoice: Total - $${total.toFixed(2)}, Tax - $${tax.toFixed(2)}, Shipping - $${shipping.toFixed(2)}, Grand Total - $${grandTotal.toFixed(2)}`);
+});
+
+// Function to calculate shipping based on total quantity
+function calculateShipping(totalQuantity) {
+    let shippingCost = 0;
+    if (totalQuantity <= 5) {
+        shippingCost = 5; // Flat rate for small quantities
+    } else if (totalQuantity <= 10) {
+        shippingCost = 10; // Higher rate for medium quantities
+    } else {
+        shippingCost = 15; // Maximum rate for large quantities
+    }
+    return shippingCost;
+}
+
 
 // Serve static files from a directory named "public"
 app.use(express.static(__dirname + '/public'));
